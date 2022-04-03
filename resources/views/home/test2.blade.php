d8eed134dcd0249240b390dc4b0bbc50eac06a68
MustafaTheEngineer <mustafaesat01@gmail.com>
2022-04-03 21:19:21 +0300

    upload-2

---
+++ resources/views/home/test2.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Blade</title>
+</head>
+<body>
+    <h1>These are the test parameters</h1>
+    <br>
+    <p>Id numbers: {{$id}}</p>
+    <p>Number Value : {{$number}}</p>
+    <p>Sum of parameters : {{$number + $id}}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+// 1- Write in route
+Route::get('/hello',function(){
+    return "Hello World";
+});
+
+//2- Call view in route
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+//3- Call controller function
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+//4- Route->Controller->View
+Route::get('/test',[HomeController::class,'test'])->name('test');
+
+//5- Route with parameters
+Route::get('/parameter/{id}/{number}',[HomeController::class,'parameter'])->name('parameter');
+
+// Route with post
+Route::post('/save',[HomeController::class,'save'])->name('save');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
